6ab7258d5f27b9ed735fb82ab1a94d6d33390882
Yashavanth6362 <140520118+Yashavanth6362@users.noreply.github.com>
2026-05-02 12:58:32 +0530

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.easy.apk',
+  appName: 'Easy APK',
+  webDir: 'www'
+};
+
+export default config;
