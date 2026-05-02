d8a8046b8f295ebfeb987b377e6742efc36fb1a9
Yashavanth6362 <140520118+Yashavanth6362@users.noreply.github.com>
2026-05-02 07:09:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,10 +10,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Simple App',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Crime App')),
-        body: const Center(child: Text('App Working!')),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
+  void increase() => setState(() => count++);
+  void decrease() => setState(() {
+        if (count > 0) count--;
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Counter App"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Counter Value",
+              style: TextStyle(fontSize: 22),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "$count",
+              style: const TextStyle(
+                  fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: decrease,
+                  child: const Text("Decrease"),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: increase,
+                  child: const Text("Increase"),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
